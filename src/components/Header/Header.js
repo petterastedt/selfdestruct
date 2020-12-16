6a47767f97b593187f6d63c828e8208985554c00
petterastedt <petter.astedt@gmail.com>
2020-12-16 22:56:56 +0100

    Added logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from './../../assets/img/logo.png'
 
 const Header = () => (
   <div className="header centerComponent">
-    <h1>privtext.me</h1>
+    <div className="header-logoWrapper">
+      <img
+        src={logo}
+        alt="privtext logo"
+        aria-label="privtext logo"
+        className="header-logo"
+      />
+      <h1>privtext.me</h1>
+    </div>
+
     <h4>share self-destructing messages anonymously</h4>
   </div>
 )
