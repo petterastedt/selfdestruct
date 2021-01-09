738b74d67a006219eea20ac904bbcc1fbf77ae31
petterastedt <petter.astedt@gmail.com>
2021-01-09 12:57:14 +0100

    contenct(About): changed content on about page

--- api/routes/api.js
+++ api/routes/api.js
@@ -18,6 +18,7 @@ const exceptionHandler = fn => (req, res, next) => {
 
 // CLEANUP ROUTE
 router.get(`/${process.env.CLEANUP_URL}`, exceptionHandler(async (req, res) => {
+  console.log('test')
   messageController.cleanupExpired()
   res.sendStatus(200)
 }))
--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -35,12 +35,14 @@ const InfoBox = () => (
           The encryption is done with <a className="link-styled" href="https://www.npmjs.com/package/mongoose-encryption" aria-label="Mongoose Encryption NPM package" alt="Mongoose Encryption NPM package" target="_blank" rel="noopener noreferrer">mongoose-encryption</a>.
           <br />
           <br />
-          If you want to get in touch feel free to send an email to <a className="link-styled" href="mailto:info@privtext.me" aria-label="email info@privtext.me">info@privtext.me</a> or visit my website at <a href="https://www.petterastedt.com" className="link-styled" aria-label="Petter Åstedt portfolio website" target="_blank" rel="noreferrer noopener">https://www.petterastedt.com</a>.
+          If you want to get in touch feel free to send an email to <a className="link-styled" href="mailto:info@privtext.me" aria-label="email info@privtext.me">info@privtext.me</a>.
         </p>
         <br />
-        <i>/ Petter</i>
+        {/* <i>/ Petter</i> */}
       </div>
       <div className="infoBox-features">
+
+
         {/* <h3>Features</h3>
         <ul>
           <li>Add sender name (NEW)</li>
