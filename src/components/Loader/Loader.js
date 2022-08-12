526ecf6d234e7d032a3db716dffae04dc19b9f69
Petter Åstedt <petter.astedt@gmail.com>
2022-08-12 20:12:30 +0200

    feat(loader): added loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from './../../assets/img/logo.png'
+
+const Loader = () => (
+  <div className="loader">
+    <img
+      alt="privtext logo"
+      aria-label="privtext logo"
+      className="loader-logo"
+      decoding="async"
+      loading="lazy"
+      src={logo}
+    />
+    <div className="loader-text">Loading</div>
+  </div>
+)
+
+export default Loader
