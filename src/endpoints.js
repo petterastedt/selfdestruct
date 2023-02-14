46235a638930d82726236915fefdc94293dde1e2
Petter Åstedt <petter.astedt@gmail.com>
2023-02-14 22:54:53 +0100

    fix(endpoints): reverse changes in endpoints.js file

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,10 +1,12 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 const endpoints = {
-  createMessage: `${isProduction || 'http://localhost:5000'}/api/post`,
-  getMessage: `${isProduction || 'http://localhost:5000'}/api/message`
+  createMessage: `${
+    isProduction ? '/api/post' : 'http://localhost:5000/api/post'
+  }`,
+  getMessage: `${
+    isProduction ? '/api/message' : 'http://localhost:5000/api/message'
+  }`
 }
-
-//
 
 export default endpoints
